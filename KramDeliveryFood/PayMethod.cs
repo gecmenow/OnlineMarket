e945327e97c41fed7e61baafcafac5ce15592826
gecmenow <gector456@gmail.com>
2021-04-20 12:49:10 +0300

    added classes for buyer and manufacturer, also, added class for products

---
+++ KramDeliveryFood/PayMethod.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace KramDeliveryFood
+{
+    public class PayMethod
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ KramDeliveryFood/PriceType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace KramDeliveryFood
+{
+    public class PriceType
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+    }
+}
