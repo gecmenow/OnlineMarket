8a640c281389618ea0c25065d27436f2adaba619
gecmenow <gector456@gmail.com>
2021-05-17 07:54:10 +0300

    fixed files according to task_3

--- KramDeliverFoodCompleted/Check/FileCheck.cs
+++ KramDeliverFoodCompleted/Check/FileCheck.cs
@@ -6,23 +6,9 @@ namespace KramDeliverFoodCompleted.Check
 {
     public class FileCheck
     {
-        public static void ProductsFileExist()
+        public static void CreateProductsFile(string path)
         {
-            var path = Path.Combine(Environment.CurrentDirectory, Variables.Folder, Variables.ProductsForOrder);
-
-            if (!File.Exists(path))
-                using (FileStream file = new FileStream(path, FileMode.Create))
-                    file.Close();
-        }
-
-        public static void FileExist(string path)
-        {
-            if (!File.Exists(path))
-            {
-                using (var file = new FileStream(path, FileMode.Create))
-
-                file.Close();
-            }
+            using var file = new StreamWriter(path, true); ;
         }
     }
 }
--- KramDeliverFoodCompleted/Interaction/BuyerReader.cs
+++ KramDeliverFoodCompleted/Interaction/BuyerReader.cs
@@ -9,22 +9,14 @@ namespace KramDeliverFoodCompleted.Interaction
         {
             var input = Console.ReadLine();
 
-<<<<<<< HEAD
-            while (!Checker.EmptyUserInput(input) && !Checker.NonStringUserInput(input) && !Checker.UserLengthInput(input))
-=======
             while (!Checker.EmptyUserInput(input) && !Checker.NonLetterUserInput(input) && !Checker.UserLengthInput(input))
->>>>>>> task_3
             {
                 Logger.RepeatInput();
 
                 input = Console.ReadLine();
             }
 
-<<<<<<< HEAD
-            var result = Converter.Converter.UserInput(input);
-=======
             int.TryParse(input, out var result);
->>>>>>> task_3
 
             return result;
         }
@@ -52,11 +44,7 @@ namespace KramDeliverFoodCompleted.Interaction
         {
             var input = Console.ReadLine();
 
-<<<<<<< HEAD
             while (!Checker.EmptyUserInput(input) == false && BuyerInfoChecker.CheckPhone(input) == false)
-=======
-            while (Checker.EmptyUserInput(input) == false)
->>>>>>> task_3
             {
                 Logger.RepeatInput();
 
@@ -70,11 +58,7 @@ namespace KramDeliverFoodCompleted.Interaction
         {
             var input = Console.ReadLine();
 
-<<<<<<< HEAD
             while (!Checker.EmptyUserInput(input) && BuyerInfoChecker.CheckAddress(input))
-=======
-            while (!Checker.EmptyUserInput(input))
->>>>>>> task_3
             {
                 Logger.RepeatInput();
 
