a98743f6b02554751acf3dddaab92b229a5ede65
gecmenow <gector456@gmail.com>
2021-06-05 12:32:54 +0300

    small fix

--- KramDeliverFoodCompleted/Service/CheckerService.cs
+++ KramDeliverFoodCompleted/Service/CheckerService.cs
@@ -8,7 +8,7 @@ namespace KramDeliverFoodCompleted.Check
         public bool CheckPhone(string number)
         {
             var pattern = @"^(\+380|0)(\(\d{2}\)|\d{2})\s?\d{3}\s?\d{2}\s?\d{2}$";
-            var expression = new Regex(pattern, RegexOptions.Compiled);
+            var expression = new Regex(pattern);
             var isMatch = expression.IsMatch(number);
 
             return isMatch;
@@ -17,7 +17,7 @@ namespace KramDeliverFoodCompleted.Check
         public bool CheckAddress(string address)
         {
             var pattern = @"(^([а-я]{5}|[а-я]{2})\s?(.|\s{1})\s?([А-Я][а-я]{9}|[А-Я][а-я]{4})(,|.)\s?([а-я]|[а-я]{3})(.|\s{1})\s?\d{2})|(,\s?([а-я]{2}|[а-я]{8})(.|\s{1})\s?\d{2})$";
-            var expression = new Regex(pattern, RegexOptions.Compiled);
+            var expression = new Regex(pattern);
             var isMatch = expression.IsMatch(address);
 
             return isMatch;
