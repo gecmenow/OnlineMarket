ab9ebe0820ba92bfae820be9232ea44549359ff5
gecmenow <gector456@gmail.com>
2021-06-08 15:00:27 +0300

    remove comments in code

--- KramDeliverFoodCompleted/Interfaces/IOrderService.cs
+++ KramDeliverFoodCompleted/Interfaces/IOrderService.cs
@@ -5,10 +5,6 @@ namespace KramDeliverFoodCompleted.Interfaces
 {
     public interface IOrderService
     {
-        //string PhoneNumber { get; set; }
-        //string Address { get; set; }
-        //IList<Product> OrderProducts { get; set; }
-
         public void AddProductToOrder(Product product);
         public IList<Order> GetOrders();
         Order GetOrder();
