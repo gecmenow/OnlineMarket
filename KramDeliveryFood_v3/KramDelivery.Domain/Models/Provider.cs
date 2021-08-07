aeebe302f3bd4ba8f12417623b1867f0bfba87de
gecmenow <gector456@gmail.com>
2021-08-07 13:24:23 +0300

    fixes

--- KramDeliveryFood_v3/KramDelivery.Domain/Models/Provider.cs
+++ KramDeliveryFood_v3/KramDelivery.Domain/Models/Provider.cs
@@ -6,6 +6,7 @@ namespace KramDelivery.Domain.Models
     {
         public Guid ProviderId { get; set; }
         public string Name { get; set; }
+        public string CompanyName { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
     }
