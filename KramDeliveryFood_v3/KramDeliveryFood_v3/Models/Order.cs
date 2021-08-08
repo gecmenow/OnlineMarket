3539510a56ff8c8ead93433d086eed90f6c6761a
gecmenow <gector456@gmail.com>
2021-08-08 09:49:45 +0300

    fixes

--- KramDeliveryFood_v3/KramDeliveryFood_v3/Models/Order.cs
+++ KramDeliveryFood_v3/KramDeliveryFood_v3/Models/Order.cs
@@ -7,13 +7,14 @@ namespace KramDeliveryFood_v3.Models
     public class Order
     {
         public Guid OrderId { get; set; }
-        public List<Product> OrderProducts = new();
+        public Guid BuyerId { get; set; }
         public Buyer Buyer { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public DateTime OrderTime { get; set; }
         public decimal FullPrice { get; set; }
-        public Deliver Delivers { get; set; }
+        public Deliver Deliver { get; set; }
         public OrderStatus OrderStatus { get; set; }
+        public List<Product> OrderProducts { get; set; }
     }
 }
--- KramDeliveryFood_v3/KramDeliveryFood_v3/Models/Provider.cs
+++ KramDeliveryFood_v3/KramDeliveryFood_v3/Models/Provider.cs
@@ -4,7 +4,7 @@ namespace KramDeliveryFood_v3.Models
 {
     public class Provider
     {
-        public Guid ProdviderId { get; set; }
+        public Guid ProviderId { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
