db7ac7bd38863a9e482035e7686f649808bac351
gecmenow <gector456@gmail.com>
2021-08-25 16:59:30 +0300

    added endpoints

--- ASP.NET/KramDeliveryFoodAPI/KramDeliveryFoodAPI/Controllers/ProductController.cs
+++ ASP.NET/KramDeliveryFoodAPI/KramDeliveryFoodAPI/Controllers/ProductController.cs
@@ -7,7 +7,7 @@ namespace KramDeliveryFoodAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class HomeController : ControllerBase
+    public class ProductController : ControllerBase
     {
         [HttpGet]
         public IList<KramDeliverFoodCompleted.Models.Product> Get()
@@ -38,6 +38,25 @@ namespace KramDeliveryFoodAPI.Controllers
             var productService = new ProductService(new StoreContext(), new LoggerService(), new SerializerService());
             productService.DeleteProduct(product);
         }
-        
+        //---Product---
+        //(Get product by id)
+        //GET Product/id
+        //(Get product by category name)
+        //GET Product/categoryName
+        //---Provider---
+        //(Add provider)
+        //POST Provider
+        //(Get provider information by id)
+        //GET Provider/id
+        //---UserOrder---
+        //(Get user order by id)
+        //GET UserOrder/id
+        //(Get user orders)
+        //GET UserOrder
+        //---Category---
+        //(Get categories)
+        //GET Category
+        //(Get category by id)
+        //GET Category/id
     }
 }
