f6e475d669fa8b9a4911cbabfffe6f1006bfa2be
gecmenow <gector456@gmail.com>
2021-09-07 19:37:42 +0300

    removed commints in PR

--- ASP.NET/KramDeliveryFoodAPI/KramDeliveryFoodAPI/Startup.cs
+++ ASP.NET/KramDeliveryFoodAPI/KramDeliveryFoodAPI/Startup.cs
@@ -24,7 +24,6 @@ namespace KramDeliveryFoodAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<IProductService, ProductService>();
@@ -37,7 +36,6 @@ namespace KramDeliveryFoodAPI
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
