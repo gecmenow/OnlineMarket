4c27e06696fa84088f46487ea219b70822db8869
gecmenow <gector456@gmail.com>
2021-09-14 13:34:39 +0300

    changed filter for showing product names from body

--- ASP.NET/KramDeliveryFoodAPI/KramDeliveryFoodAPI/Filters/RequestBodyFilter.cs
+++ ASP.NET/KramDeliveryFoodAPI/KramDeliveryFoodAPI/Filters/RequestBodyFilter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KramDelivery.Structure.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
 using System.Collections;
@@ -13,13 +14,26 @@ namespace KramDeliveryFoodAPI.Filters
         public void OnActionExecuted(ActionExecutedContext context)
         {
             var result = context.Result as ObjectResult;
+            var type = result.Value.GetType().GenericTypeArguments[0].Name;
 
-            var value = ((IEnumerable)result.Value).Cast<object>().ToList();
+            if (type == typeof(Product).Name)
+            { 
+                var product = ((IEnumerable)result.Value).Cast<Product>().ToList();
 
-            foreach(var data in value)
+                foreach (var data in product)
+                {
+                    Console.WriteLine(data.Name);
+                }
+            }
+            else
             {
-                Console.WriteLine(data);
-            } 
+                var value = ((IEnumerable)result.Value).Cast<object>().ToList();
+
+                foreach (var data in value)
+                {
+                    Console.WriteLine(data);
+                }
+            }
         }
 
         public void OnActionExecuting(ActionExecutingContext context)
