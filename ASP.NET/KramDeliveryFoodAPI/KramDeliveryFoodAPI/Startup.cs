bfa0418049440b7063837a4b6e1a362f0ccc9898
gecmenow <gector456@gmail.com>
2021-09-15 15:27:51 +0300

    fix

--- ASP.NET/KramDeliveryFoodAPI/KramDeliveryFoodAPI/Startup.cs
+++ ASP.NET/KramDeliveryFoodAPI/KramDeliveryFoodAPI/Startup.cs
@@ -6,17 +6,9 @@ using KramDeliveryFood.Structure.Interfaces.Repositories;
 using KramDeliveryFoodAPI.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace KramDeliveryFoodAPI
 {
